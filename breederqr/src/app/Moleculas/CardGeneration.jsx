54366add9e9b1843c058402952ade1d17bffa586
monocoto3000 <128664121+monocoto3000@users.noreply.github.com>
2023-10-21 23:49:54 -0600

    Cards y AppBar Avances

---
+++ breederqr/src/app/Moleculas/CardGeneration.jsx
@@ -0,0 +1,46 @@
+"use client"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import React from 'react';
+import Cards from './Cards';
+import { Grid } from '@mui/material';
+
+export default function CardGeneration() {
+    let pacientes = [
+        {
+            nombre: "pablo",
+            edad: 12,
+            sexo: "H",
+            img: "https://www.gstatic.com/webp/gallery3/1.sm.png"
+        },
+        {
+            nombre: "pepe",
+            edad: 23,
+            sexo: "M",
+            img: "https://placebear.com/g/200/200"
+        },
+        {
+            nombre: "cesar",
+            edad: 9,
+            sexo: "H",
+            img: "https://source.unsplash.com/user/c_v_r/1900×800"
+        },
+    ]
+    return (
+        <>
+            <Grid container direction="row" >
+                {pacientes.map((paciente, index) => {
+                    return (
+                        <div key={index}>
+                            <Grid margin={2} width={300}>
+                                <Cards aux={paciente} />
+                            </Grid>
+                        </div>
+                    );
+                })}
+            </Grid>
+        </>
+    )
+}
--- breederqr/src/app/page.js
+++ breederqr/src/app/page.js
@@ -1,11 +1,9 @@
-import Image from 'next/image'
 import styles from './page.module.css'
-import Text from './Atoms/Text'
-
+import Ejemplares from './pages/Ejemplares'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Text/>
+      <Ejemplares />
     </main>
   )
 }
